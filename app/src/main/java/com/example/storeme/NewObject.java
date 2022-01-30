70d40abca11e41b2bc44c97c11084ae6aa405bfb
bbati <bruno.batisse@yahoo.fr>
2022-01-30 17:07:52 +0100

    Ajouter attributs des objects ajoutés dans StoreMe + un espace texte et logique d'affichage Ajouter bouton add et cancel qui ne font rien

--- app/src/main/java/com/example/storeme/NewObject.java
+++ app/src/main/java/com/example/storeme/NewObject.java
@@ -3,8 +3,12 @@ package com.example.storeme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -31,5 +35,23 @@ public class NewObject extends Activity {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         typeSpinner.setAdapter(myAdapter);
+
+        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView no_attribute1_Text = (TextView) findViewById(R.id.no_attribute1_label);
+                no_attribute1_Text.setText(getResources().getStringArray(R.array.attribute1_string)[position]);
+                no_attribute1_Text.setVisibility(View.VISIBLE);
+                TextView no_attribute2_Text = (TextView) findViewById(R.id.no_attribute2_label);
+                no_attribute2_Text.setText(getResources().getStringArray(R.array.attribute2_string)[position]);
+                no_attribute2_Text.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
